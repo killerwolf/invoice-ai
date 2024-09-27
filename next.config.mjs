7ecbac00c08031798c186c33498f0f5002197c5d
Hamdi LAADHARI <killerwolf06@gmail.com>
2024-09-27 18:44:48 +0200

    testing gh-pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // Specify the base path if your repository name is not the same as the GitHub Pages URL
+  basePath: '/invoice-ai',
+};
 
 export default nextConfig;
